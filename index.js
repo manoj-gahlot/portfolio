ca58add83fca283ae42fb2e0c9a07c066f4cf88f
Manoj <2020uee1424@mnit.ac.in>
2023-05-25 13:04:07 +0530

    Remake all content

---
+++ index.js
@@ -0,0 +1,19 @@
+// alert("hello")
+// $(document).on("mousemove", function (event) {
+//     event.pageX
+//     // console.log("x-index " + event.pageX);
+//     // console.log("y-index " + event.pageY);
+// })
+
+// $(".social-logos li a").on("mousemove", function (event) {
+//     var item = $(this).attr("id");
+//     console.log(item);
+//     var target = $("#" + item + " svg");
+//     console.log(target);
+//     target.css("box-shadow", "0px 0px  1rem #dddddd")
+//     setTimeout(function () {
+//         // target.css("fill", "currentColor")
+//         target.css("box-shadow", " ");
+
+//     }, 500);
+// })
